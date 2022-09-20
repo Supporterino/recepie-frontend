d1ebce19e2a00251a23779e255d1c1f81b9a03fa
Supporterino <lars@roth-kl.de>
2022-09-20 16:32:26 +0200

    skrew grid for moment

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 import { getAllReceipes } from '../services/requests';
 import { Recipe } from '../types';
@@ -8,7 +8,7 @@ import FlexBox from '../components/layout/FlexContainer';
 import CardWide from '../components/cards/CardWide';
 
 const Home: React.FunctionComponent = () => {
-  const { isLoading, isError, error, data } = useQuery(['receipes'], getAllReceipes);
+  const { isLoading, isError, error, data } = useQuery(['recipes'], getAllReceipes);
 
   if (isLoading)
     return (
@@ -25,19 +25,24 @@ const Home: React.FunctionComponent = () => {
     );
 
   return (
-    <Grid
-      container
-      justifyContent="space-evenly"
-      alignItems="center"
-      spacing={{ xs: 2, md: 3 }}
-      columns={{ xs: 12, sm: 12, md: 12 }}
-    >
+    // <Grid
+    //   container
+    //   justifyContent="space-evenly"
+    //   alignItems="center"
+    //   spacing={{ xs: 2, md: 3 }}
+    //   columns={{ xs: 12, sm: 12, md: 12 }}
+    // >
+    //   {data.map((recipe: Recipe, index: number) => (
+    //     <Grid item xs={11} sm={2} md={2} key={index}>
+    //       <CardWide recipe={recipe} />
+    //     </Grid>
+    //   ))}
+    // </Grid>
+    <Box>
       {data.map((recipe: Recipe, index: number) => (
-        <Grid item xs={11} sm={2} md={2} key={index}>
-          <CardWide recipe={recipe} />
-        </Grid>
+        <CardWide recipe={recipe} />
       ))}
-    </Grid>
+    </Box>
   );
 };
 
